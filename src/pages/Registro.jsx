0beef5881eb488acadb8808607efa1ab8b00e97c
ErlinChavez <erlinsaulchavezyarango44@gmail.com>
2026-04-11 15:13:49 -0500

    cambios

--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "../styles/Register.css";
+import "../styles/Registro.css";
 import bg from "../assets/clinic.jpg";
 
 function Register() {
